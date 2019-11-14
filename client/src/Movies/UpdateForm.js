cf33df3bede32ece7fb4f641bb8ab54c79515da0
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-11-14 13:43:45 -0800

    UpdateForm.js updateForm fn completed

--- client/src/Movies/UpdateForm.js
+++ client/src/Movies/UpdateForm.js
@@ -11,6 +11,25 @@ const UpdateForm = props => {
     stars: []
   };
 
+  //props param passes in id from url
+  //movie state holds movie being updated, uses initialMovie const as initial Value
+
+  const updateForm = props => {
+    const [movie, setMovie] = useState(initialMovie);
+    const changeHandler = e => {
+      //e.persist(); <-- needed??
+      //holds value of changed movie property input on form
+      let value = e.target.value;
+      if (e.target.name === "id" || "metascore") {
+        value = parseInt(value, 10);
+      }
+      setMovie({
+        ...movie,
+        [e.target.name]: value
+      });
+    };
+  };
+
   return (
     <div>
       <form>
